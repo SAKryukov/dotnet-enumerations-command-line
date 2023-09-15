2d987c818d29ed5d5b320d2a6dc423ff33407e4b
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-15 09:33:26 -0400

    Code: Demo: refined documentation

--- code/Demo/CommandLine.Demo/Main/DefinitionSet.cs
+++ code/Demo/CommandLine.Demo/Main/DefinitionSet.cs
@@ -14,6 +14,20 @@
             internal const int timeout = 1000; // 1s //SA???
         } //class Sampler
 
+        internal static class Documentation {
+            const string indent = "          ";
+            const string delimiter = ",";
+            const string suffix = ":";
+            const string switchBnf = "{+|-}";
+            internal static string FormatName(string name, bool last, bool isSwitch = false) =>
+                $"-{name}{(isSwitch ? switchBnf : string.Empty)}{(last ? suffix : delimiter)}";
+            internal static string FormatDescripton(string name) =>
+                $"{indent}{name}";
+            internal static string FormatDisplayName(string name, string description) =>
+                $"{indent}{name}: {description}";
+        } //class Documentation
+
+
     } //class DefinitionSet
 
 }
--- code/Demo/CommandLine.Demo/View/WindowDocumentation.xaml.cs
+++ code/Demo/CommandLine.Demo/View/WindowDocumentation.xaml.cs
@@ -12,22 +12,22 @@
             Universal.Enumerations.Enumeration<Main.StringOption> values = new();
             StringList stringSwitches = new();
             foreach (var option in switches)
-                ShowOption(stringSwitches, option);
+                ShowOption(stringSwitches, option, isSwitch: true);
             treeViewItemSwitches.ItemsSource = stringSwitches;
             StringList stringValues = new();
             foreach (var option in values)
-                ShowOption(stringValues, option);
+                ShowOption(stringValues, option, isSwitch: false);
             treeViewItemValues.ItemsSource = stringValues;
         } //WindowDocumentation
 
-        void ShowOption<ENUM>(StringList list, Universal.Enumerations.EnumerationItem<ENUM> option) {
-            list.Add($"-{option.Name}");
+        void ShowOption<ENUM>(StringList list, Universal.Enumerations.EnumerationItem<ENUM> option, bool isSwitch = false) {
+            list.Add(Main.DefinitionSet.Documentation.FormatName(option.Name, option.AbbreviatedName == option.Name, isSwitch: isSwitch));
             if (option.AbbreviatedName != option.Name)
-                list.Add($"-{option.AbbreviatedName}");
+                list.Add(Main.DefinitionSet.Documentation.FormatName(option.AbbreviatedName, true, isSwitch: isSwitch));
             if (!string.IsNullOrEmpty(option.Description))
-                list.Add($"            {option.DisplayName}: {option.Description}");
+                list.Add(Main.DefinitionSet.Documentation.FormatDisplayName(option.DisplayName, option.Description));
             else
-                list.Add($"            {option.DisplayName}");
+                list.Add(Main.DefinitionSet.Documentation.FormatDescripton(option.DisplayName));
         } //ShowOption
 
         protected override void OnClosing(CancelEventArgs e) {
